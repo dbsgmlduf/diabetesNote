931e480d6991bc130ba3074348dcb3c83d223193
HuiyeolYun <huiyy9211@gmail.com>
2024-07-06 01:25:19 +0900

    과거 데이터 조회 페이지: 각 기능별 페이지 제작

--- client/lib/mainPages/settingscreen.dart
+++ client/lib/mainPages/settingscreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../history/pastdatapage.dart';
+
 class SettingScreen extends StatelessWidget {
   final String userName = '사용자 이름'; // 이 부분은 추후 DB와 연결하여 사용자 데이터를 가져올 예정입니다.
   final String userAge = '사용자 나이'; // 이 부분은 추후 DB와 연결하여 사용자 데이터를 가져올 예정입니다.
@@ -38,10 +40,12 @@ class SettingScreen extends StatelessWidget {
               ],
             ),
             SizedBox(height: 40),
-            _buildSettingCard(context, '개인정보 수정', Icons.person, _navigateToPersonalInfo),
-            _buildSettingCard(context, '과거 데이터 조회', Icons.history, _navigateToDataHistory),
-            _buildSettingCard(context, '라이센스', Icons.book, _navigateToLicense),
-            _buildSettingCard(context, '문의하기', Icons.contact_mail, _navigateToContact),
+            _buildSettingCard(context, '개인정보 수정', Icons.person, () {}),
+            _buildSettingCard(context, '과거 데이터 조회', Icons.history, () {
+              Get.to(PastDataPage());
+            }),
+            _buildSettingCard(context, '라이센스', Icons.book, () {}),
+            _buildSettingCard(context, '문의하기', Icons.contact_mail, () {}),
           ],
         ),
       ),
@@ -57,21 +61,5 @@ class SettingScreen extends StatelessWidget {
         onTap: onTap,
       ),
     );
-  }
-
-  void _navigateToPersonalInfo() {
-    // 추후 개인정보 수정 페이지로 이동
-  }
-
-  void _navigateToDataHistory() {
-    // 추후 과거 데이터 조회 페이지로 이동
-  }
-
-  void _navigateToLicense() {
-    // 추후 라이센스 페이지로 이동
-  }
-
-  void _navigateToContact() {
-    // 추후 문의하기 페이지로 이동
   }
 }
