87abd4e3b6d81862630c01d1f693c591b12518a1
HuiyeolYun <huiyy9211@gmail.com>
2024-07-05 12:51:55 +0900

    기능별 디렉토리 및 파일분리

---
+++ client/lib/detailfeature/injectionpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InjectionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '기능 2',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
