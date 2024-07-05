8f9f686ceee2efa9e0cad62339828b067cbcdb8d
HuiyeolYun <huiyy9211@gmail.com>
2024-07-05 14:31:52 +0900

    인슐린 기능 설계

--- client/lib/detailfeature/injectionpage.dart
+++ client/lib/detailfeature/injectionpage.dart
@@ -1,12 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class InjectionPage extends StatelessWidget {
+class InjectionPage extends StatefulWidget {
+  @override
+  _InjectionPageState createState() => _InjectionPageState();
+}
+
+class _InjectionPageState extends State<InjectionPage> {
+  Map<int, bool> buttonStates = {};
+  String lastInjectionDate = '';
+  bool isInjectionCompletedToday = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadButtonStates();
+    _checkForMonthChange();
+  }
+
+  _checkForMonthChange() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int currentMonth = DateTime.now().month;
+    int savedMonth = prefs.getInt('savedMonth') ?? currentMonth;
+
+    if (currentMonth != savedMonth) {
+      setState(() {
+        buttonStates.clear();
+        prefs.setInt('savedMonth', currentMonth);
+      });
+      prefs.remove('buttonStates');
+    }
+  }
+
+  _loadButtonStates() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      lastInjectionDate = prefs.getString('lastInjectionDate') ?? '';
+      buttonStates = _stringToMap(prefs.getString('buttonStates') ?? '');
+      isInjectionCompletedToday = prefs.getBool('isInjectionCompletedToday') ?? false;
+    });
+  }
+
+  _saveButtonStates() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('lastInjectionDate', lastInjectionDate);
+    prefs.setInt('savedMonth', DateTime.now().month);
+    prefs.setString('buttonStates', _mapToString(buttonStates));
+    prefs.setBool('isInjectionCompletedToday', isInjectionCompletedToday);
+  }
+
+  Map<int, bool> _stringToMap(String mapString) {
+    if (mapString.isEmpty) {
+      return {};
+    }
+    return Map<int, bool>.fromEntries(
+      mapString.split(',').map((entry) {
+        List<String> keyValue = entry.split(':');
+        return MapEntry(int.parse(keyValue[0]), keyValue[1] == 'true');
+      }),
+    );
+  }
+
+  String _mapToString(Map<int, bool> map) {
+    return map.entries.map((e) => '${e.key}:${e.value}').join(',');
+  }
+
+  _handleInjectionComplete() {
+    setState(() {
+      bool allCompleted = true;
+      for (int i = 1; i <= 32; i++) {
+        if (buttonStates[i] != true) {
+          buttonStates[i] = true;
+          allCompleted = false;
+          break;
+        }
+      }
+      if (allCompleted) {
+        buttonStates.clear();
+      }
+      lastInjectionDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+      isInjectionCompletedToday = true;
+    });
+    _saveButtonStates();
+  }
+
+  bool _canCompleteInjectionToday() {
+    final todayDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    return lastInjectionDate != todayDate && !isInjectionCompletedToday;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '기능 2',
-        style: TextStyle(fontSize: 24),
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text('오늘의 인슐린', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.lightBlue,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  child: Image.asset('assets/image/injectionBackground.png'),
+                ),
+                Positioned(
+                  top: 100,
+                  child: Column(
+                    children: List.generate(4, (rowIndex) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(8, (colIndex) {
+                          int index = rowIndex * 8 + colIndex + 1;
+                          return GestureDetector(
+                            onTap: () => _handleInjectionComplete(),
+                            child: Container(
+                              margin: EdgeInsets.all(4.0),
+                              child: CircleAvatar(
+                                backgroundColor:
+                                buttonStates[index] == true ? Colors.green : Colors.grey,
+                                child: Text('$index'),
+                              ),
+                            ),
+                          );
+                        }),
+                      );
+                    }),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              lastInjectionDate.isNotEmpty
+                  ? '마지막 인슐린 주사 날짜: $lastInjectionDate'
+                  : '아직 주사 기록이 없습니다.',
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            SizedBox(height: 10), // 텍스트와 버튼 사이의 간격을 조절
+            ElevatedButton(
+              onPressed: _canCompleteInjectionToday() ? _handleInjectionComplete : null,
+              child: Text('오늘 주입 완료'),
+            ),
+          ],
+        ),
       ),
     );
   }
